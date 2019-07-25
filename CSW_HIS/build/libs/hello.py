3bdc2cbedc77c53396da48a1871e581c0c5d918b
rafa98neves <rafaelalexandrerodriguesneves@gmail.com>
2019-07-25 14:22:26 +0100

    robot tests passed

--- CSW_HIS/build/libs/hello.py
+++ CSW_HIS/build/libs/hello.py
@@ -12,8 +12,16 @@ def send_request(endpoint,name):
 	print URL
 	return send(URL, PARAMS)
 
+def send_newUser(endpoint,name,age,address):
+	URL = "http://localhost:8080/" + endpoint
+	PARAMS = {'name':name,'age':age,'address':address}
+	print URL
+	return str(send(URL,PARAMS))
+
 def send(URL,PARAMS):
 	r = requests.get(url = URL, params = PARAMS)
 	return r.status_code
 
-
+def post(URL,PARAMS):
+	r = requests.post(url = URL, params = PARAMS)
+	return r.status_code
--- CSW_HIS/src/main/java/CSW_HIS/SetuserController.java
+++ CSW_HIS/src/main/java/CSW_HIS/SetuserController.java
@@ -16,23 +16,31 @@ public class SetuserController {
     		@RequestParam(value="address", required=false) String address){
     	
 		    	Pattern pattern = Pattern.compile("[a-zA-Z0-9_]*");
-		       	Matcher matcher = pattern.matcher(name);
+		       	Matcher name_matcher = pattern.matcher(name);
 		       	
-	       		if(name==null) {
+		       	pattern = Pattern.compile("[a-zA-Z0-9\\ _º,-]*");
+		       	Matcher address_matcher = pattern.matcher(address);
+		       	address_matcher.replaceAll(" ");
+		       	
+	       		if(name==null || name.equals("")) {
 	       			return new ResponseEntity<>("Insert a name please!", HttpStatus.BAD_REQUEST);
 	       		}
-	       		else if(!matcher.matches()) {
+	       		else if(!name_matcher.matches()) {
 	       			return new ResponseEntity<>("Invalid characters in your name!", HttpStatus.BAD_REQUEST);
 	       		}
-	       		else if(age==null) {
+	       		else if(age==null || age.isEmpty()) {
 	       			return new ResponseEntity<>("Insert an age please!", HttpStatus.BAD_REQUEST);
-	       		}
-	       		else if(address==null) {
+	       		} 
+	       		else if(address==null || address.equals("")) {
 	       			return new ResponseEntity<>("Insert an address please!", HttpStatus.BAD_REQUEST);
+	       		}
+	       		else if(!address_matcher.matches()) {
+	       			return new ResponseEntity<>("Address contains some \"special characters\" (Natalia's orders, sorry!)", HttpStatus.BAD_REQUEST);
 	       		}
 	       		else if(!age.matches("\\d+")) {
 	       			return new ResponseEntity<>("Age must be numeric", HttpStatus.BAD_REQUEST);
 	       		}
+	       		
 
     			Connect c = new Connect();
     			c.Insert(String.format("INSERT INTO users(name,age,address) VALUES (\"%s\",%d,\"%s\")",name,Integer.parseInt(age), address));
